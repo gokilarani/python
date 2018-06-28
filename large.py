318ba421f0e4c874a9c12ad237efa0a41264a683
gokilarani <32609108+gokilarani@users.noreply.github.com>
2018-06-28 11:06:01 +0100

    Add files via upload

---
+++ large.py
@@ -0,0 +1,9 @@
+a=input()
+b=input()
+c=input()
+if (int(a)>int(b)) and (int(a)>int(c)):
+      print('a is great')
+elif (int(b)>int(c)):
+        print('b is great')
+else:
+    print('c is great')
